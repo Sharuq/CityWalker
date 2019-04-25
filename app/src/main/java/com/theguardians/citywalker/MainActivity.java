bef4b72c59ecde1a5f2a58b317eb06547573261e
sharuq <mkun0005@student.monash.edu>
2019-04-26 03:11:32 +1000

    Initial commit

---
+++ app/src/main/java/com/theguardians/citywalker/MainActivity.java
@@ -0,0 +1,102 @@
+package com.theguardians.citywalker;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private static final String TAG = "MainActivity";
+    private static final int ERROR_DIALOG_REQUEST = 9001;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_main);
+        Toolbar toolbar = findViewById (R.id.toolbar);
+        setSupportActionBar (toolbar);
+
+        init ();
+        if(isServicesOK()) {
+            init ();
+        }
+        FloatingActionButton fab = findViewById (R.id.fab);
+        fab.setOnClickListener (new View.OnClickListener () {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make (view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction ("Action", null).show ();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater ().inflate (R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId ();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected (item);
+    }
+    private void init(){
+
+        Button btnMap = (Button) findViewById(R.id.searchRouteBtn);
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,RouteActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    public boolean isServicesOK(){
+        Log.d(TAG, "isServicesOK: checking services version");
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+        if(available== ConnectionResult.SUCCESS)
+        {
+            //everything is fine user can map request
+            Log.d(TAG, "isServicesOK: Google play services is working");
+            return true;
+        }
+        else if(GoogleApiAvailability.getInstance().isUserResolvableError(available))
+        {
+            //error occured  but we can be resolved
+            Log.d(TAG, "isServicesOK: an error occured but we can fix it");
+            Dialog dialog =GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this,available,ERROR_DIALOG_REQUEST);
+            dialog.show();
+
+        }
+        else
+        {
+            Toast.makeText(this,"You can't make request", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+}
