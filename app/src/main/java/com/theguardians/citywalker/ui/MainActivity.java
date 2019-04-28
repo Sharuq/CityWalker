d5081840244fd181a9607991d00451943087b3f2
sharuq <mkun0005@student.monash.edu>
2019-04-28 23:31:57 +1000

    checks_done and UI done

--- app/src/main/java/com/theguardians/citywalker/ui/MainActivity.java
+++ app/src/main/java/com/theguardians/citywalker/ui/MainActivity.java
@@ -1,10 +1,19 @@
 package com.theguardians.citywalker.ui;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -19,11 +28,20 @@ import com.google.android.gms.common.GoogleApiAvailability;
 import com.theguardians.citywalker.R;
 import com.theguardians.citywalker.RouteActivity;
 
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+
 public class MainActivity extends AppCompatActivity {
 
 
     private static final String TAG = "MainActivity";
     private static final int ERROR_DIALOG_REQUEST = 9001;
+
+    private static final int  PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 9003;
+    private static final int PERMISSIONS_REQUEST_ENABLE_GPS = 9002;
+    private boolean mLocationPermissionGranted = false;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +50,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById (R.id.toolbar);
         setSupportActionBar (toolbar);
 
-        //init ();
-        if(isServicesOK()) {
-            init ();
-        }
+       context =this;
         FloatingActionButton fab = findViewById (R.id.fab);
         fab.setOnClickListener (new View.OnClickListener () {
             @Override
@@ -62,10 +77,104 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent =  new Intent(this,AboutUsActivity.class);
+            startActivity(intent);
             return true;
         }
 
         return super.onOptionsItemSelected (item);
+    }
+
+    /**
+     * OnResume Override
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        new CheckInternetConnection().execute();
+
+    }
+
+    /**
+     * Check if internet connection is available or not through async task
+     */
+
+    public class CheckInternetConnection extends AsyncTask<Void,Void, Boolean> {
+
+
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+            try {
+                int timeoutMs = 1500;
+                Socket sock = new Socket();
+                SocketAddress sockaddr = new InetSocketAddress ("8.8.8.8", 53);
+
+                sock.connect(sockaddr, timeoutMs);
+                sock.close();
+
+                return true;
+            }
+            catch (Exception e) {
+
+                return false;
+
+
+
+            }
+        }
+
+
+
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+
+            if (aBoolean) {
+
+                //isAppOpenedFirstTime();
+                checkLocationServices();
+            }
+            else {
+
+                //Toast.makeText(this, "No Internet Connection", Toast.LENGTH_SHORT).show();
+                new AlertDialog.Builder(context)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("Closing the App")
+                        .setMessage("No Internet Connection,check your settings")
+                        .setPositiveButton("Close", new DialogInterface.OnClickListener()
+                        {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+
+                        })
+                        .show();
+            }
+        }
+    }
+
+
+    /**
+     * This methid check if the lccation permisission is enabled or not and
+     * will ask them to be granted it its not given
+     */
+    private void checkLocationServices() {
+
+        if (checkMapServices()) {
+
+            if (mLocationPermissionGranted) {
+
+                getNextActivity();
+            }
+            else {
+
+                getLocationPermission();
+            }
+
+        }
+
+
     }
     private void init(){
 
@@ -78,27 +187,156 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    public boolean isServicesOK(){
-        Log.d(TAG, "isServicesOK: checking services version");
-        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
-        if(available== ConnectionResult.SUCCESS)
-        {
-            //everything is fine user can map request
-            Log.d(TAG, "isServicesOK: Google play services is working");
-            return true;
-        }
-        else if(GoogleApiAvailability.getInstance().isUserResolvableError(available))
-        {
-            //error occured  but we can be resolved
-            Log.d(TAG, "isServicesOK: an error occured but we can fix it");
-            Dialog dialog =GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this,available,ERROR_DIALOG_REQUEST);
-            dialog.show();
 
-        }
-        else
-        {
-            Toast.makeText(this,"You can't make request", Toast.LENGTH_SHORT).show();
+    public void getNextActivity(){
+
+        Button btnMap = (Button) findViewById(R.id.searchRouteBtn);
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RouteActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    /**
+     *  Check whether maps service is available
+     */
+
+    private boolean checkMapServices(){
+        if(isServicesOK()){
+            if(isMapsEnabled()){
+                return true;
+            }
         }
         return false;
     }
+
+
+    /**
+     * No GPS alert message
+     */
+    private void buildAlertMessageNoGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("This application requires GPS to work properly, do you want to enable it?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        Intent enableGpsIntent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivityForResult(enableGpsIntent, PERMISSIONS_REQUEST_ENABLE_GPS);
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+
+    /**
+     * Checck if location is enabled
+     * @return
+     */
+    public boolean isMapsEnabled(){
+        final LocationManager manager = (LocationManager) getSystemService( Context.LOCATION_SERVICE );
+
+        if ( !manager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
+            buildAlertMessageNoGps();
+            return false;
+        }
+        return true;
+    }
+
+
+    /**
+     * Ask for location permission
+     */
+    private void getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true;
+            getNextActivity();
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+
+    /**
+     * Is all services okay and app ready for use
+     * @return
+     */
+    public boolean isServicesOK(){
+
+
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+
+        if(available == ConnectionResult.SUCCESS){
+            //everything is fine and the user can make map requests
+
+            return true;
+        }
+        else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            //an error occured but we can resolve it
+
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }else{
+            Toast.makeText(this, "You can't make map requests", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
+
+    /**
+     * On Request permission callback override
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        mLocationPermissionGranted = false;
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mLocationPermissionGranted = true;
+                }
+            }
+        }
+    }
+
+
+    /**
+     * Calling location granting activity for result
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ENABLE_GPS: {
+                if(mLocationPermissionGranted){
+                    getNextActivity();
+                }
+                else{
+                    getLocationPermission();
+                }
+            }
+        }
+
+    }
+
 }
