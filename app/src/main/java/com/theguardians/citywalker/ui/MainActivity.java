33a042718d06698d45455e735873c583dddc7075
sharuq <mkun0005@student.monash.edu>
2019-04-29 22:01:58 +1000

    Update_done

--- app/src/main/java/com/theguardians/citywalker/ui/MainActivity.java
+++ app/src/main/java/com/theguardians/citywalker/ui/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -41,13 +42,13 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
     private static final int ERROR_DIALOG_REQUEST = 9001;
-
-
+    private static final int MY_PERMISSIONS_REQUEST_CALL_PHONE=1;
     private static final int MY_PERMISSIONS_REQUEST_SEND_SMS =0 ;
     private static final int  PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 9003;
     private static final int PERMISSIONS_REQUEST_ENABLE_GPS = 9002;
     private boolean mLocationPermissionGranted = false;
     private boolean mSMSPermissionGranted = false;
+    private boolean mCallPermissionGranted = false;
     private Context context;
     private List<UserContact> contacts;
     @Override
@@ -66,8 +67,20 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener (new View.OnClickListener () {
             @Override
             public void onClick(View view) {
-                Snackbar.make (view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction ("Action", null).show ();
+
+                if(contacts.size ()>0) {
+                    String phNo = contacts.get (0).getPhoneNumber ();
+                    System.out.println (phNo);
+                    if (mCallPermissionGranted) {
+                        checkCallServices (phNo);
+                    } else {
+                        getCallPermission (phNo);
+                    }
+                }
+                else{
+                     Snackbar.make (view, "Please Add an Emergency Contact Number", Snackbar.LENGTH_LONG)
+                       .setAction ("Action", null).show ();
+                }
             }
         });
     }
@@ -179,13 +192,24 @@ public class MainActivity extends AppCompatActivity {
                 getNextActivity();
             }
             else {
-
                 getLocationPermission();
             }
 
         }
 
 
+    }
+
+
+
+    private void checkCallServices(String phNo){
+        if(mCallPermissionGranted){
+
+            callNumber (phNo);
+        }
+        else {
+            getCallPermission (phNo);
+        }
     }
 
     private  void checkSMSServices(){
@@ -196,6 +220,32 @@ public class MainActivity extends AppCompatActivity {
         }
         else {
             getSMSPermission ();
+        }
+    }
+
+    public void callNumber(String phoneNumber) {
+        Intent intent = new Intent (Intent.ACTION_CALL);
+        intent.setData (Uri.parse ("tel:" + phoneNumber));
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission (this,
+                Manifest.permission.CALL_PHONE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions (this,
+                    new String[]{Manifest.permission.CALL_PHONE},
+                    MY_PERMISSIONS_REQUEST_CALL_PHONE);
+
+            // MY_PERMISSIONS_REQUEST_CALL_PHONE is an
+            // app-defined int constant. The callback method gets the
+            // result of the request.
+        } else {
+            //You already have permission
+            try {
+                startActivity (intent);
+            } catch (SecurityException e) {
+                e.printStackTrace ();
+            }
+
         }
     }
     public void getNextActivity(){
@@ -292,6 +342,31 @@ public class MainActivity extends AppCompatActivity {
                     PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
         }
     }
+    private void getCallPermission(String phNo) {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CALL_PHONE)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.CALL_PHONE)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CALL_PHONE},
+                        MY_PERMISSIONS_REQUEST_CALL_PHONE);
+
+            }
+
+            Intent intent = new Intent (Intent.ACTION_CALL);
+            intent.setData (Uri.parse ("tel:" + phNo));
+            startActivity (intent);
+        }
+        else {
+
+            Intent intent = new Intent (Intent.ACTION_CALL);
+            intent.setData (Uri.parse ("tel:" + phNo));
+            startActivity (intent);
+        }
+
+    }
 
     protected void getSMSPermission() {
 
@@ -353,11 +428,30 @@ public class MainActivity extends AppCompatActivity {
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     mLocationPermissionGranted = true;
                 }
+                else {
+
+                    Toast.makeText(this, "Location permission not granted", Toast.LENGTH_SHORT).show();
+                }
             }
             case MY_PERMISSIONS_REQUEST_SEND_SMS: {
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     mSMSPermissionGranted =true;
+                }else {
+
+                    Toast.makeText(this, "SMS permission not granted", Toast.LENGTH_SHORT).show();
+                }
+            }
+            case MY_PERMISSIONS_REQUEST_CALL_PHONE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    mCallPermissionGranted = true;
+
+                } else {
+
+                    Toast.makeText(this, "Call permission not granted", Toast.LENGTH_SHORT).show();
                 }
             }
         }
